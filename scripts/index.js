e37b4997765b7441a8b332ac18b6ab6e110e0afe
Fair Lucas <lukitaa.27@gmail.com>
2023-05-02 16:51:18 -0300

    funciona en pags sin bloqueador de anuncios

---
+++ scripts/index.js
@@ -0,0 +1,34 @@
+import { Contador } from "./contador.js";
+
+const partidaEnMemoria = JSON.parse(localStorage.getItem("truco"));
+const p1Nombre= "Nosotros";
+const p2Nombre= "Ellos";
+const modal= document.querySelector("dialog");
+
+const p1 = new Contador(
+  p1Nombre,
+  document.getElementById("jugador1Container"),
+  partidaEnMemoria && partidaEnMemoria[p1Nombre] ? partidaEnMemoria[p1Nombre] : 0
+);
+const p2 = new Contador(
+    p2Nombre,
+    document.getElementById("jugador2Container"),
+    partidaEnMemoria && partidaEnMemoria[p2Nombre] ? partidaEnMemoria[p2Nombre] : 0
+);
+
+document.querySelector("#reset").addEventListener("click", ()=> modal.showModal())
+document.querySelector("#volver").addEventListener("click", ()=> modal.close())
+document.querySelector("#aceptar").addEventListener("click", ()=> {
+    p1.reset();
+    p2.reset();
+    modal.close()
+})
+
+document.querySelectorAll("button").forEach(boton => boton.addEventListener("click", guardarPartida))
+function guardarPartida(){
+    const partidaAGuardar = {
+        [p1Nombre]: p1.numero,
+        [p2Nombre]: p2.numero,
+    }
+    localStorage.setItem("truco",JSON.stringify(partidaAGuardar))
+}
